bec219765aaa9e35d76d8b6c3b60d02635897d7b
insistenceZHUO <1340282952@qq.com>
2019-09-30 17:18:54 +0800

    初始化基础模板

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 
 import './index.less'
- 
+//  这个是antd初始化基础模板
 
 const testHOC = (wrappedComponent) => {
     return class HOCComponent extends Component {
